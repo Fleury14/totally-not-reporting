9476d9d351c8956b6f3ecf27df86399c2e33052d
J.R. Ruggiero <fleury14@comcast.net>
2018-06-19 14:45:24 -0700

    feat(db): database populates with csv, list api edited

--- server/api/endpoints/test-list-endpoint.ts
+++ server/api/endpoints/test-list-endpoint.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import * as db from './../../db/db';
 
 export function listTest(req: Request, res: Response, next: NextFunction) {
-    db.any(' SELECT * FROM teams', [true]).then( (data) => {
+    db.any(' SELECT * FROM movies_meta', [true]).then( (data) => {
         res.json({message: 'List found', result: data})
     })
 }
