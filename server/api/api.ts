bc539777b8715547841cf9c7179877274ccc241b
J.R. Ruggiero <fleury14@comcast.net>
2018-06-20 10:41:32 -0700

    feat(api): enpoint for searching by year. requires a number

--- server/api/api.ts
+++ server/api/api.ts
@@ -4,6 +4,7 @@ import { testBodyEndpoint } from "./endpoints/test-body-endpoint";
 import { listTest } from "./endpoints/test-list-endpoint";
 import { search } from "./endpoints/search-title-endpoint";
 import { topTen } from "./endpoints/top-10-vote-endpoint";
+import { getByYear } from "./endpoints/get-by-year-endpoint";
 
 export function initAPI (app:Application) {
     app.route('/api/test').get(testEndpoint);
@@ -13,4 +14,5 @@ export function initAPI (app:Application) {
     app.route('/api/search-title').post(search);
 
     app.route('/api/top10').post(topTen);
+    app.route('/api/get-by-year').post(getByYear);
 }
