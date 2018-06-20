8e04b543b69c41a0959a68b8cdcfbbddfbf2c90e
J.R. Ruggiero <fleury14@comcast.net>
2018-06-20 15:07:59 -0700

    chore(httpservice): http service created for post requests, accounts for OS'

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { SideBarComponent } from './components/sidebar/sidebar.component';
 import { RoutingComponents, AppRoutingModule } from './app.routing.module';
+import { SearchService } from './services/search.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent, NavComponent, SideBarComponent, RoutingComponents
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, HttpClientModule
   ],
-  providers: [],
+  providers: [ SearchService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
