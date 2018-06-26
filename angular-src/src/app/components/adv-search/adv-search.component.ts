2ff1a9030f7cc6eb9745b5a063a2d0fb9ba55354
J.R. Ruggiero <fleury14@comcast.net>
2018-06-26 15:59:10 -0700

    feat(search): form portion of adv search

--- angular-src/src/app/components/adv-search/adv-search.component.ts
+++ angular-src/src/app/components/adv-search/adv-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { CatergorySelection } from '../../interfaces/category-selection';
+import { IDynamicRequest } from '../../interfaces/dynamic-request';
 
 @Component({
     selector: 'app-adv-search',
@@ -9,8 +11,72 @@ import { NgForm } from '@angular/forms';
 
 export class AdvSearchComponent {
     @ViewChild('searchForm') private _searchForm: NgForm;
+    public searchCategory: CatergorySelection[] = [{
+        name: "Budget",
+        ref: "budget",
+        type: "number"
+    }, {
+        name: "Original Title",
+        ref: "original_title",
+        type: "text"
+    }, {
+        name: "Overview",
+        ref: "overview",
+        type: "text"
+    }, {
+        name: "Popularity",
+        ref: "original_title",
+        type: "number"
+    }, {
+        name: "Release Year",
+        ref: "release_date",
+        type: "number"
+    }, {
+        name: "Revenue",
+        ref: "revenue",
+        type: "number"
+    }, {
+        name: "Run Time",
+        ref: "run_time",
+        type: "number"
+    }, {
+        name: "Tag Line",
+        ref: "tag_line",
+        type: "text"
+    }, {
+        name: "Title",
+        ref: "title",
+        type: "text"
+    }, {
+        name: "Vote Average",
+        ref: "vote_average",
+        type: "number"
+    }, {
+        name: "Vote Count",
+        ref: "vote_count",
+        type: "number"
+    }];
 
-    public whatAmI() {
-        console.log(this._searchForm);
+    public selectedCategory: string;
+    public categoryValueNum: number;
+    public requestedColumns: IDynamicRequest = {
+        adult: false,
+        budget: false,
+        movie_id: false,
+        original_title: false,
+        overview: false,
+        popularity: false,
+        release_date: false,
+        revenue: false,
+        runtime: false,
+        tagline: false,
+        title: false,
+        vote_average: false,
+        vote_count: false
     }
+
+    public submit(value) {
+        console.log('submitting...', value, this.requestedColumns);
+    }
+
 }
