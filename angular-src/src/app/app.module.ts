1e611ea7d03d2f35795f8b9afc511714851cf8f9
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-07-05 15:12:44 -0700

    fixed rebase

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DragulaModule } from 'ng2-dragula';
 
 // import { ModalComponent } from './components/modal/modal.component';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { AppMaterialModule } from './material.module';
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, FormsModule, AppRoutingModule, AppMaterialModule,
-    HttpClientModule, ReactiveFormsModule
+    HttpClientModule, ReactiveFormsModule, DragulaModule
+
   ],
   providers: [ SearchService, HttpService, RecentSearchService ],
   entryComponents: [ ModalComponent ],
