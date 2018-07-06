32fc2f191f33744fb1f8c202c2002917bb15328a
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-07-06 16:01:47 -0700

    push after rebase with develop part 2

--- angular-src/src/app/components/sidebar/sidebar.component.ts
+++ angular-src/src/app/components/sidebar/sidebar.component.ts
@@ -22,7 +22,6 @@ export class SideBarComponent implements DoCheck {
     ngDoCheck() {
         this.recentArray = this._recent.getSearchList();
         const screenSize = window.innerHeight;
-<<<<<<< HEAD
         if(screenSize < 800 ){
             this.sizedRecentArray = this.recentArray.slice(0,6);
         } else if(screenSize < 900){
@@ -32,17 +31,6 @@ export class SideBarComponent implements DoCheck {
 
         }else {
             this.sizedRecentArray = this.recentArray.slice(0,this.recentArray.length);
-=======
-        if (screenSize < 800 ) {
-            this.sizedRecentArray = this.recentArray.slice(0, 6);
-        } else if (screenSize < 900) {
-            this.sizedRecentArray = this.recentArray.slice(0, 8);
-        } else if (screenSize < 1000) {
-            this.sizedRecentArray = this.recentArray.slice(0, 10);
-
-        } else {
-            this.sizedRecentArray = this.recentArray.slice(0, this.recentArray.length);
->>>>>>> feat(adv-search): stepper w/ drag and drop
         }
     }
 
