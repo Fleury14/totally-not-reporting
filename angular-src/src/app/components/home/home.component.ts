880f6dc39c7fbe258ce507c391506d32e756d680
J.R. Ruggiero <fleury14@comcast.net>
2018-07-09 16:42:53 -0700

    refactor(home): budget, revenue, and release date show data from entire db

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../../services/search.service';
+import { IMovie } from '../../interfaces/movie';
 // import { IMovie } from '../../interfaces/movie';
 
 @Component({
@@ -9,11 +11,17 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit{
     ngOnInit(): void {
+        this._search.getYearRange(1900, 2017).subscribe(response => {
+            if(response.result) {
+                this.allMovieResults = response.result;
+                this.setRevenueData(this.allMovieResults);
+                this.setBudgetData(this.allMovieResults);
+                this.setReleaseDateData(this.allMovieResults);
+
+            }
+        })
         //different graph methods
-        this.setBudgetData();
-        this.setRevenueData();
         this.setRuntimeData();
-        this.setReleaseDateData();
         this.setKidFriendlyData();
     }
     // different graph variables
@@ -31,6 +39,7 @@ export class HomeComponent implements OnInit{
     public whiteColorScheme = {
         domain: ['#fff']
     };
+    public allMovieResults: IMovie[];
 
     // budget
     public xAxis = true;
@@ -50,67 +59,70 @@ export class HomeComponent implements OnInit{
     public explodeSlices = false;
     public doughnut = false;
 
+    constructor (private _search: SearchService) {}
 
-    public setBudgetData() {
-        this.budgetData =[{
-            name: "1",
-            value: 5422
+
+    public setBudgetData(movies:IMovie[]) {
+        this.budgetData = [{
+            name: "40m",
+            value: 0
         },{
-            name: "2",
-            value: 8912
+            name: "80m",
+            value: 0
         },{
-            name: "3",
-            value: 2500
+            name: "120m",
+            value: 0
         },{
-            name: "4",
-            value: 3652
+            name: "160m",
+            value: 0
         },{
-            name: "5",
-            value: 924
+            name: "2000m",
+            value: 0
         },{
-            name: "6",
-            value: 1623
+            name: "240m",
+            value: 0
         },{
-            name: "7",
-            value: 963
-        },{
-            name: "8",
-            value: 3692
-        },{
-            name: "9",
-            value: 952
-        },{
-            name: "10",
-            value: 4520
+            name: "280m",
+            value: 0
         }];
+        movies.forEach( (movie:IMovie) => {
+            if (movie.budget > 30000000) {
+                let movieIndex = Math.floor(movie.budget / 40000000) - 1;
+                movieIndex = movieIndex > 6 ? 6 : movieIndex;
+                // console.log(movieIndex, this.budgetData[movieIndex]['value']);
+                if (this.budgetData[movieIndex] !== undefined) {
+                    this.budgetData[movieIndex]['value']++;
+                }
+            }
+            
+        })
+        
     }
 
-    public setRevenueData(){
-        this.revenueData = [{
-            name: "1",
-            value: 5421
-        },{
-            name: "2",
-            value: 8912
-        },{
-            name: "3",
-            value: 2500
-        },{
-            name: "4",
-            value: 3652
-        },{
-            name: "5",
-            value: 924
-        },{
-            name: "6",
-            value: 1623
-        },{
-            name: "7",
-            value: 963
-        },{
-            name: "8",
-            value: 3692
-        }];
+    public setRevenueData(movies:IMovie[]){
+        this.revenueData = [];
+        movies.forEach(movie => {
+            if (movie.budget && movie.revenue && movie.title) {
+                this.revenueData.push({name: movie.title, value: movie.revenue - movie.budget});
+            }
+        } );
+        this.revenueData.sort( (a, b) => b.value - a.value);
+        if (this.revenueData.length > 8) {
+            this.revenueData.splice(8);
+        }
+        // console.log('tree data lol', this.revenueData);
+    }
+
+    public millions(num: number): string {
+        if (typeof num != 'number') {
+            if (num['value']) { num = num['value']}
+            else if (num['cell']) { num = num['cell']['value']}
+        } // account for the graphs not passing a number
+        if(num < 1000000000) {
+            return '$' + Math.round(num * 10 / 1000000) / 10 + 'm';
+        } else {
+            return '$' + Math.round(num * 10 / 1000000000) / 10 + 'b';
+        }
     }
 
     public setRuntimeData() {
@@ -208,40 +220,39 @@ export class HomeComponent implements OnInit{
               ]
             }
           ]
+          
     }
 
-    public setReleaseDateData(){
+    public setReleaseDateData(movies: IMovie[]){
         this.releaseDateData = [{
-            name: "1",
-            value: 5422
+            name: "50s",
+            value: 0
         },{
-            name: "2",
-            value: 8912
+            name: "60s",
+            value: 0
         },{
-            name: "3",
-            value: 2500
+            name: "70s",
+            value: 0
         },{
-            name: "4",
-            value: 3652
+            name: "80s",
+            value: 0
         },{
-            name: "5",
-            value: 924
+            name: "90s",
+            value: 0
         },{
-            name: "6",
-            value: 1623
+            name: "00s",
+            value: 0
         },{
-            name: "7",
-            value: 5963
-        },{
-            name: "8",
-            value: 3692
-        },{
-            name: "9",
-            value: 2952
-        },{
-            name: "10",
-            value: 9520
+            name: "10s",
+            value: 0
         }];
+        movies.forEach( (movie:IMovie) => {
+            let movieDate = new Date(movie.release_date);
+            let index = Math.floor( movieDate.getFullYear() / 10 ) - 195;
+            if (index >= 0 ) {
+                this.releaseDateData[index]['value']++;
+            }
+        })
     }
 
     public setKidFriendlyData() {
