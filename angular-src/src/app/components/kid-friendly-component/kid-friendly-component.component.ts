932b2c10ddc5392927db78c8aeffeba992fea863
David Ramirez <dramirez2096@gmail.com>
2018-07-10 11:06:18 -0700

    chore(kid-friendly): slider brought in

--- angular-src/src/app/components/kid-friendly-component/kid-friendly-component.component.ts
+++ angular-src/src/app/components/kid-friendly-component/kid-friendly-component.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ErrorStateMatcher, MatSnackBar } from '@angular/material';
+import { FormGroupDirective, NgForm, FormControl, Validators } from '@angular/forms';
+import { IMovie } from '../../interfaces/movie';
 
 @Component({
   selector: 'app-kid-friendly-component',
@@ -6,6 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kid-friendly-component.component.scss']
 })
 export class KidFriendlyComponent implements OnInit {
+  @ViewChild('yearForm') private _yearForm: NgForm;
+    public startYear: number;
+    public endYear: number;
+    public searchResults: IMovie[];
+    public showResults = false;
+
+    // SLIDER OPTIONS
+    public yearValue = [1990, 1995]
+    public sliderConfig: any = {
+        behaviour: 'drag',
+        connect: true,
+        margin: 1,
+        limit: 5, // NOTE: overwritten by [limit]="10"
+        step: 1,
+        range: {
+          min: 1960,
+          max: 2017
+        },
+        pips: {
+            mode: 'count',
+            density: 2,
+            values: 7,
+            stepped: true
+        }
+      };
 
   constructor() { }
 
