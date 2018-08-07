034e3b7898a7d5131cb575fb29ff0c0551691f63
J.R. Ruggiero <fleury14@comcast.net>
2018-08-07 11:02:25 -0700

    chore(service/crm-detail): added getter/setter to service, initialized crm-detail page

---
+++ angular-src/src/app/components/crm-detail/crm-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-crm-detail',
+    templateUrl: './crm-detail.component.html',
+    styleUrls: [ './crm-detail.component.scss' ]
+})
+
+export class CRMDetailComponent {
+    
+}
--- angular-src/src/app/services/crm.data.service.ts
+++ angular-src/src/app/services/crm.data.service.ts
@@ -7,9 +7,19 @@ import { Observable } from 'rxjs';
 
 export class CRMDataService {
 
+    private selectedClient: IClient;
+
     constructor( private _http: HttpService ) {}
 
     public getClients(): Observable<any> {
         return this._http.get('crm/get-all-clients');
+    }
+
+    public set selectClient(client: IClient) {
+        this.selectClient = client;
+    }
+
+    public get getClient(): IClient {
+        return this.selectedClient
     }
 }
