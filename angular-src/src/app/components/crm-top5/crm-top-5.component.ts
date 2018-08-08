59178293c50b6acc8883e111fa6e779269ba06f5
J.R. Ruggiero <fleury14@comcast.net>
2018-08-08 13:41:58 -0700

    chore(top5):top5 component created and added to router

---
+++ angular-src/src/app/components/crm-top5/crm-top-5.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-crm-top5',
+    templateUrl: './crm-top-5.component.html',
+    styleUrls: [ './crm-top-5.component.scss' ]
+})
+
+export class CRMTop5Component {
+    
+}
